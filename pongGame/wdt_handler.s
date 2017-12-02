959593ffb5391a99b50b7082d84d7983765ed1c1
=Fernando Martinez <=fmartinez14@miners.utep.edu>
2017-12-02 13:57:17 -0700

    pushing new files

--- pongGame/wdt_handler.s
+++ pongGame/wdt_handler.s
@@ -6,9 +6,10 @@
 	.word	WDT
 	.text
 	
-
+ 	
 	.extern redrawScreen
 	.extern wdt_c_handler
+        
 WDT:
 ; start of function
 ; attributes: interrupt 
@@ -56,3 +57,4 @@ ball_no_move:
 	.local	count
 	.comm	count,1,1
 	.ident	"GCC: (GNU) 4.9.1 20140707 (prerelease (msp430-14r1-364)) (GNUPro 14r1) (Based on: GCC 4.8 GDB 7.7 Binutils 2.24 Newlib 2.1)"
+
